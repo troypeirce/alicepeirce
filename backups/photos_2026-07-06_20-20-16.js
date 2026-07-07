431675a956397eae873fa3b66af0a77950758fa4
Troy Peirce <troypeirce9@gmail.com>
2026-07-06 22:17:34 -0600

    Replace website with updated local version

---
+++ backups/photos_2026-07-06_20-20-16.js
@@ -0,0 +1,19 @@
+/* ==========================================
+   Alice Peirce Photo Albums
+   Apple Photos Structure
+========================================== */
+
+const albums = {
+
+    "March 2026": [
+],
+    "April 2026": [
+	],
+    "July 2026": [
+	],
+
+
+    "August 2026": [
+
+    ]
+};
